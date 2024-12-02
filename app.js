517a9fda26c9a1f443825d6eed989383c447ce80
Nathan MACAIGNE <nathan.macaigne@hotmail.com>
2024-12-02 14:47:25 +0100

    ajouter date

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
+app.get('/date', (req, res) => {
+    res.send("current date: " + new Date().toDateString());
+});
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 });
